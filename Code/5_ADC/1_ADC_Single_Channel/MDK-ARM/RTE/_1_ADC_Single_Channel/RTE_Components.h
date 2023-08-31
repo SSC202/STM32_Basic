a4ab5b0947b0a53cbadcf3dd90c4f1e45a88d8d6
SSC202 <e22750706642022@163.com>
2023-08-31 20:10:16 +0800

    STM32 ADC实验

---
+++ Code/5_ADC/1_ADC_Single_Channel/MDK-ARM/RTE/_1_ADC_Single_Channel/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '1_ADC_Single_Channel' 
+ * Target:  '1_ADC_Single_Channel' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
