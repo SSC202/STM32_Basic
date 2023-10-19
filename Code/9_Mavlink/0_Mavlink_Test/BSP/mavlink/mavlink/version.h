dd1b6dacae29294a09b54bc4b800b745c9f70453
SSC202 <e22750706642022@163.com>
2023-10-19 15:41:16 +0800

    Mavlink 测试

---
+++ Code/9_Mavlink/0_Mavlink_Test/BSP/mavlink/mavlink/version.h
@@ -0,0 +1,14 @@
+/** @file
+ *  @brief MAVLink comm protocol built from mavlink.xml
+ *  @see http://mavlink.org
+ */
+#pragma once
+ 
+#ifndef MAVLINK_VERSION_H
+#define MAVLINK_VERSION_H
+
+#define MAVLINK_BUILD_DATE "Fri Oct 06 2023"
+#define MAVLINK_WIRE_PROTOCOL_VERSION "1.0"
+#define MAVLINK_MAX_DIALECT_PAYLOAD_SIZE 24
+ 
+#endif // MAVLINK_VERSION_H
